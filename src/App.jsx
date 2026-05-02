5180fd1493185d30e40a67b80d74d0b32e0de4d9
aidan-smagh <aidansmagh@gmail.com>
2026-05-02 12:56:49 -0400

    import font

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import profile_picture from '../public/profile_picture.jpeg'
 
 function App() {
   return (
-    <div className="flex items-center bg-blue-500 px-4 py-3 text-white hover:bg-blue-400">
+    <div className="flex justify-around">
+      {/*
       <a 
         href="Aidan_Smagh_Resume.pdf"
         target="_blank"
-        className="hover:underline"
+        className="bg-blue-500 px-4 py-3 text-white hover:bg-blue-400 w-64 rounded-md"
       >
-        View Resume
+        View Resumehttp://localhost:5173/public/profile_picture.jpeg
       </a>
-
+      */}
+      <div className="mt-30 font-noto">
+        <h1>Hello!</h1>
+        <h1>My name is <span className="font-bold">Aidan Smagh</span></h1>
+      </div>
+      <img src={profile_picture} className="w-64 h-64 rounded-full object-cover mt-50"/>
     </div>
     );
 }
